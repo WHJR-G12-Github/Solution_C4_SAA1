ddabcabfecf7cf800b3e3df8b3dde5b07338f14d
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-14 12:28:41 +0530

    Create solution_c4_saa1.py

---
+++ solution_c4_saa1.py
@@ -0,0 +1,8 @@
+# Define a function 'product()' with 'num1','num2' as parameters
+def product(num1,num2):
+  # Calculate the 'result' by multiplying 'num1' and 'num2'
+  result = num1*num2
+  # Return the 'result'
+  return result
+# Call the function 'product()' to multiply '29' and '34' and print the  returned 'result'
+print(product(29,34))
